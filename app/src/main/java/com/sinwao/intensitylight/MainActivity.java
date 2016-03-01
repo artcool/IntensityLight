5373379f8277541622d94f5ee6c75dd9ee37297f
artcool <710760186@qq.com>
2016-03-01 13:45:59 +0800

    Initial commit

---
+++ app/src/main/java/com/sinwao/intensitylight/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sinwao.intensitylight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 显示当前的光照强度
+ * 作者：artcool
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
